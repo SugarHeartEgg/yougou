651768139c336e468810a0dcc6cdef26a4417681
LianYiHeng <1330489329@qq.com>
2018-09-12 13:47:49 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="header">
+      <!-- 搜索框 -->
+      <icon type="search" size="16"></icon>
+      <input type="text" placeholder="搜索">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less"> 
+  .header {
+    padding: 20rpx 16rpx;
+    position: relative;
+    background-color: #ff2d4a;
+    icon {
+      position: absolute;
+      top:34rpx;
+      left: 50%;
+      transform: translateX(-200%);
+      z-index: 998;
+    }
+    input {
+      height: 60rpx;
+      border-radius: 10rpx;
+      background-color: white;
+      width: 100%;
+      color: #bbb;
+      text-align: center;
+      font-size: 24rpx;
+    }
+  }
+</style>
