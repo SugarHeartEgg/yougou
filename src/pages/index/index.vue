53d3c6e42c25b98dfb7f0a9ed57652ccc6e1bc7c
LianYiHeng <1330489329@qq.com>
2018-09-12 16:03:38 +0800

    封装ajax，全局调用

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,35 +3,76 @@
     <div class="header">
       <!-- 搜索框 -->
       <icon type="search" size="16"></icon>
-      <input type="text" placeholder="俊武好帅">
+      <input type="text" placeholder="搜索">
     </div>
+    <!-- 轮播图 -->
+    <swiper indicator-dots autoplay circular>
+      <swiper-item v-for="(item,index) in swiperList" :key="item.name">
+        <image mode="aspectFill" :src="item.image_src"></image>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
-export default {}
+import tool from "../../utils/index.js";
+
+export default {
+    // 数据
+    data: function() {
+        return {
+            // 轮播图数据
+            swiperList: []
+        };
+    },
+    // 生命周期函数
+    created() {
+        // 获取轮播图数据
+        tool
+            .thenAjax({
+                url: "api/public/v1/home/swiperdata"
+            })
+            .then(res => {
+                // 绑定数据
+                // console.log(res);
+                this.swiperList = res.data.message;
+            });
+    }
+};
 </script>
 
-<style lang="less"> 
-  .header {
+<style lang="less">
+.header {
     padding: 20rpx 16rpx;
     position: relative;
     background-color: #ff2d4a;
-    icon {
-      position: absolute;
-      top:34rpx;
-      left: 50%;
-      transform: translateX(-200%);
-      z-index: 998;
+    > icon {
+        position: absolute;
+        top: 34rpx;
+        left: 50%;
+        transform: translateX(-200%);
+        z-index: 998;
     }
     input {
-      height: 60rpx;
-      border-radius: 10rpx;
-      background-color: white;
+        height: 60rpx;
+        border-radius: 10rpx;
+        background-color: white;
+        width: 100%;
+        color: #bbb;
+        text-align: center;
+        font-size: 24rpx;
+    }
+}
+// 轮播图
+swiper {
+  height: 340rpx;
+  swiper-item {
+    image {
+      display: block;
       width: 100%;
-      color: #bbb;
-      text-align: center;
-      font-size: 24rpx;
+      height: 100%;
     }
   }
+}
+
 </style>
--- src/utils/index.js
+++ src/utils/index.js
@@ -18,7 +18,27 @@ export function formatTime (date) {
   return `${t1} ${t2}`
 }
 
+// 抽取自己的 ajax 使用 Promise
+function thenAjax(opt){
+  // 返回一个 promise对象即可
+  return new Promise((resolve,reject)=>{
+    // 进行异步的操作
+    wx.request({
+      url:'https://autumnfish.cn/wx/'+opt.url,
+      data:opt.data|| {},
+      header:opt.header|| {'content-type':'application/json'},
+      method:opt.method|| 'GET',
+      dataType:opt.dataType|| 'json',
+      responseType:opt.responseType|| 'text',
+      success: resolve,
+      fail: reject,
+      complete: ()=>{}
+    });
+  })
+}
+
 export default {
   formatNumber,
-  formatTime
+  formatTime,
+  thenAjax
 }
